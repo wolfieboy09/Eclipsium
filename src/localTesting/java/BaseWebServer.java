1ea42387dcf773e31b3a9e6d358b3ba8d7b73885
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-12-26 12:36:11 -0500

    moved

---
+++ src/localTesting/java/BaseWebServer.java
@@ -0,0 +1,7 @@
+import dev.wolfieboy09.eclipsium.webpages.WebsiteProvider;
+
+public class BaseWebServer extends WebsiteProvider {
+    public BaseWebServer(int port) {
+        super(port);
+    }
+}
